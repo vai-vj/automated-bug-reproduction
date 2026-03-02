d264d5da76b4bfbdd435fb1a192e5299a3eb69eb
Vaishnavi Vijay <vaishaishvj@gmail.com>
2026-03-02 03:41:22 -0500

    removed excess comments

--- app/llm_client.py
+++ app/llm_client.py
@@ -30,33 +30,3 @@ Incident report:
     )
 
     return response.choices[0].message.content
-
-
-
-
-
-# def call_llm(incident_text: str) -> str:
-#     prompt = f"""
-# You are a software QA assistant.
-
-# Given the following incident report, return a valid JSON with the exact keys:
-# 1. reproduction_steps: list of strings
-# 2. preconditions: list of strings
-# 3. expected_behavior: string
-# 4. actual_behavior: string
-
-# Do not include any explanations, numbers, or extra text. Only return the JSON object.
-
-# Incident report:
-# {incident_text}
-# """
-
-#     response = client.chat.completions.create(
-#         model="gpt-4o-mini",
-#         response_format={"type": "json_object"},
-#         # message=[
-#         #     {"role": "user", "content": prompt}
-#         # ]
-#     )
-
-#     return response.choices[0].message.content.strip() #strip to remove any leading/trailing whitespace
--- app/main.py
+++ app/main.py
@@ -3,38 +3,45 @@ from app.llm_client import call_llm
 from app.models import BugReportOutput
 from dotenv import load_dotenv
 import json
-
-load_dotenv()
 import os
 
-print("OPENAI_API KEY exists:", bool(os.getenv("OPENAI_API_KEY"))) #debugging
+#get OPENAI_API_KEY from .env file
+load_dotenv()
+print("OPENAI_API KEY exists:", bool(os.getenv("OPENAI_API_KEY"))) #debugging print
 
 app = FastAPI(title="Automated Bug Reproduction")
 
+#confirm server is running at root directory
 @app.get("/")
 def root():
     return {"status": "ok"}
 
-
+#call LLM on sample bug report and return result
 @app.get("/debug-llm")
 def debug_llm():
+    #read sample bug report from file
     with open("data/sample_bug.txt", "r") as f:
         report_text = f.read()
 
+    #call LLM function and store raw AI output
     llm_output = call_llm(report_text)
-    print("LLM Output:", repr(llm_output))  #debug print
+    print("LLM Output:", repr(llm_output))  #debugging print
 
-    # llm_json = json.loads(llm_output)    # Convert to a Python dict
-
+    #return original report and structured LLM output as JSON
     return {
         "report": report_text,
         "llm_output": llm_output
     }
 
+#validate JSON using Pydantic model
 @app.get("/test-json")
 def test_json():
-    report = "Submit button crashes the app when clicked without filling the form."
-    response_text = call_llm(report)
+    ####report = "Submit button crashes the app when clicked without filling the form."
+    #read sample bug report from file
+    with open("data/sample_bug.txt", "r") as f:
+        report_text = f.read()
+        
+    response_text = call_llm(report_text)
 
     def clean_json(text):
         text = text.strip()
